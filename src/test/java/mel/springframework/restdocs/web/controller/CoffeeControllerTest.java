3a0db6b7f6da81e652ad7026ef1335e4a54adb18
melikebakir <melikeyaman.bakir@gmail.com>
2021-04-18 21:34:57 +0100

    index.adoc is added and controller test is updated.

--- src/test/java/mel/springframework/restdocs/web/controller/CoffeeControllerTest.java
+++ src/test/java/mel/springframework/restdocs/web/controller/CoffeeControllerTest.java
@@ -6,24 +6,33 @@ import mel.springframework.restdocs.repositories.CoffeeRepository;
 import mel.springframework.restdocs.web.model.CoffeeDto;
 import mel.springframework.restdocs.web.model.CoffeeStyleEnum;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.xml.crypto.dsig.spec.XPathFilterParameterSpec;
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static  org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(CoffeeController.class)
 @ComponentScan(basePackages = "mel.springframework.restdocs.web.mappers")
 public class CoffeeControllerTest {
@@ -41,8 +50,11 @@ public class CoffeeControllerTest {
     void getCoffeeById() throws Exception {
         given(coffeeRepository.findById(any())).willReturn(Optional.of(Coffee.builder().build()));
 
-        mockMvc.perform(get("/api/v1/coffee/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/coffee/{coffeeId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        .andDo(document("v1/coffee", pathParameters(
+                parameterWithName("coffeeId").description("UUID of desired coffee to get.")
+        )));
     }
 
     @Test
